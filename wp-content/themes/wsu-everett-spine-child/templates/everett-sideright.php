c7e2b193035960fbc94f3c56c8b0610b5aee3b15
jeremyfelt <jeremy.felt@gmail.com>
2014-03-07 16:41:41 -0800

    Add page template for Everett home

---
+++ wp-content/themes/wsu-everett-spine-child/templates/everett-sideright.php
@@ -0,0 +1,38 @@
+<?php /* Template Name: Everett Home */ ?>
+
+<?php get_header(); ?>
+
+	<main class="spine-sideright-template">
+
+		<?php if ( have_posts() ) : while( have_posts() ) : the_post(); ?>
+
+			<?php get_template_part('parts/headers'); ?>
+			<section class="row single">
+
+				<div class="column one">
+					<div class="fill-width"><div class="main_header_graphic"></div></div>
+				</div><!--/column-->
+
+			</section>
+			<section class="row sidebar">
+
+				<div class="column one">
+
+					<?php get_template_part('articles/article'); ?>
+
+				</div><!--/column-->
+
+				<div class="column two">
+
+					<?php
+					$column = get_post_meta( get_the_ID(), 'column-two', true );
+					if( ! empty( $column ) ) { echo $column; }
+					?>
+
+				</div>
+
+			</section>
+		<?php endwhile; endif; ?>
+	</main>
+
+<?php get_footer(); ?>
